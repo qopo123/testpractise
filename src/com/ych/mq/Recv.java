b7bfb0a59682cfc21d5a1c54a6be1c5f5ad4451e
yangchuanhua <yangchuanhua@yonyou.com>
2017-05-18 09:12:19 +0800

    .

--- src/com/ych/mq/Recv.java
+++ src/com/ych/mq/Recv.java
@@ -1,7 +1,35 @@
 package com.ych.mq;
 
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 /**
  * Created by yangchuanhua on 2017/5/17.
  */
 public class Recv {
+
+  private final static String QUEUE_NAME = "hello";
+
+  public static void main(String[] args) throws IOException, TimeoutException {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+    Consumer consumer = new DefaultConsumer(channel) {
+      @Override
+      public void handleDelivery(String consumerTag, Envelope envelope,
+          AMQP.BasicProperties properties, byte[] body)
+          throws IOException {
+        String message = new String(body, "UTF-8");
+        System.out.println(" [x] Received '" + message + "'");
+      }
+    };
+    channel.basicConsume(QUEUE_NAME, true, consumer);
+
+  }
 }
